25b3d894038d14810b6f08db200cfe8ef64d190f
Chau Vu <vuhachau2412@gmail.com>
2025-02-15 16:32:52 -0800

    login + mainpage frontend

--- frontend/src/CreateUser.js
+++ frontend/src/CreateUser.js
@@ -20,7 +20,7 @@ function CreateUser() {
 
       if (response.ok) {
         localStorage.setItem("username", formData.username); // Store username
-        navigate("/chat"); // Redirect to chat page
+        navigate("/mainpage"); // Redirect to chat page
       }
     } catch (error) {
       console.error("Error creating user:", error);
@@ -28,13 +28,66 @@ function CreateUser() {
   };
 
   return (
-    <div>
-      <h1>Create User</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Name" value={formData.name} onChange={handleChange} required />
-        <input type="number" name="age" placeholder="Age" value={formData.age} onChange={handleChange} required />
-        <input type="text" name="username" placeholder="Username" value={formData.username} onChange={handleChange} required />
-        <button type="submit">Create User</button>
+<div style={{ textAlign: "center", paddingTop: "5rem" }}>
+
+      <h1>Hi, new User!</h1>
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "300px",
+          margin: "auto",
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="number"
+          name="age"
+          placeholder="Age"
+          value={formData.age}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="medication"
+          placeholder="Medication"
+          value={formData.medication}
+          onChange={handleChange}
+          required
+        />
+
+        <button
+          type="submit"
+          style={{
+            marginTop: "10px",
+            padding: "8px",
+            background: "#007bff",
+            color: "#5CB286",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+        >
+          Create User
+        </button>
       </form>
     </div>
   );
